b73f5de2c59e5b67a1463eeb4182471ef2c0f249
hakimuddinhh <hakimuddinhh@gmail.com>
2021-07-29 20:06:04 +0530

    Page enhancement for desktop and mobile

--- src/pages/feed/feed.styled.tsx
+++ src/pages/feed/feed.styled.tsx
@@ -1,24 +1,25 @@
 import styled from 'styled-components';
 
+export const StyledPageContainer = styled.main`
+height: 100vh;
+transition: height 0.5s ease-in;
+overflow-y: scroll;
+`
 
 export const StyledContainer = styled.section`
 color: white;
 `;
 
-export const StyledListingContainer = styled.main`
-height: calc(100vh - 159px);
-transition: height 0.5s ease-in;
-overflow-y: scroll;
-margin: 15px 10%;
+export const StyledListingContainer = styled.section`
+max-width: 600px;
+margin: 0 auto;
+
+margin: 0 auto;
 gap: 60px;
 display: flex;
 flex-direction: column;
-padding: 10px;
+padding-top: 70px;
 
-@media screen and (max-aspect-ratio: 13/9) {
-    margin: 15px 0;
-    padding: 10px 0;
-}
 
 `;
 
